d253506a068477b0ab9216440fcb6960aad9195e
OleksiiMelnyk22 <97alexxxey97@gmail.com>
2026-04-23 22:06:19 +0300

    fix pathes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter basename="/react-senja">
+      <BrowserRouter basename="/senja-react">
           <App />
       </BrowserRouter>
   </StrictMode>,
